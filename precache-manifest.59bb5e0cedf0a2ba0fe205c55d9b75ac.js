d0e74b6d7eb7bc69c87c277b7b421fb30ff7322f
Jay <34258275+sell@users.noreply.github.com>
2020-04-11 15:04:12 -0400

    Updates

---
+++ precache-manifest.59bb5e0cedf0a2ba0fe205c55d9b75ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eda03c1faaa58ec4fdec25a722152ad",
+    "url": "/covid-Tracker/index.html"
+  },
+  {
+    "revision": "732e9bfcede8be6c1ca2",
+    "url": "/covid-Tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e50108edccf0a1e47223",
+    "url": "/covid-Tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "732e9bfcede8be6c1ca2",
+    "url": "/covid-Tracker/static/js/2.90a20473.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-Tracker/static/js/2.90a20473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50108edccf0a1e47223",
+    "url": "/covid-Tracker/static/js/main.e042bbe9.chunk.js"
+  },
+  {
+    "revision": "8024f276d515ee8c591b",
+    "url": "/covid-Tracker/static/js/runtime-main.df188aa2.js"
+  }
+]);
